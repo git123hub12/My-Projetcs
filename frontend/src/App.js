27a11259e80ea59c9fb410feeec8afc5dbab5fa2
git123hub12 <101474376+git123hub12@users.noreply.github.com>
2024-08-12 16:48:56 -0500

    Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello from React frontend!
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
